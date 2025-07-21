3d236d85fb187b50e98761e45499aca8030de2d4
bestcb2333 <2892709432@qq.com>
2025-07-21 21:44:57 +0800

    style: 调整标签顺序

--- src/App.vue
+++ src/App.vue
@@ -24,12 +24,12 @@ getWebName()
 </script>
 
 <template>
+  <div class="fixed top-0 left-0 w-full z-[999]">
+    <nav-bar />
+  </div>
+  <div class="h-[48px]"> </div>
   <el-config-provider :button="{ round: true }">
-    <div class="fixed top-0 left-0 w-full z-[999]">
-      <nav-bar />
-    </div>
-    <div class="h-[48px]"> </div>
-    <router-view/>
-    <app-footer v-if="!$route.meta.noFooter" />
+    <router-view></router-view>
   </el-config-provider>
+  <app-footer v-if="!$route.meta.noFooter"></app-footer>
 </template>
